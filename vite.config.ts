0e419a48aacee0d00cf6045dee86008ce797b2d5
helperzhou <103308958+helperzhou@users.noreply.github.com>
2025-02-24 09:34:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+        port: process.env.PORT ? Number(process.env.PORT) : 3000, // Use Azure's assigned port
+        host: '0.0.0.0', // Required for external access
+    }
 });
