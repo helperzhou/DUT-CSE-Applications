b3499614d934b459a9cb7b578fb79dfd3db5f13e
helperzhou <103308958+helperzhou@users.noreply.github.com>
2025-02-26 22:23:23 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,23 +3,17 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown
 			out: 'build',
-			precompress: false,
-			envPrefix: '',
-			polyfill: true
+			precompress: false
 		}),
 		alias: {
-			"@/*": "./path/to/lib/*",
+			"$lib": "./src/lib", // ✅ Fix alias to match your structure
+			"$routes": "./src/routes",
+			"$components": "./src/lib/components"
 		}
 	}
 };
